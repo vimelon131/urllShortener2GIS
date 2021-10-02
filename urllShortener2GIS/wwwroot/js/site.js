750587730495648cfb29b41ce379960a18481fcc
vlad <vimelon131@gmail.com>
2021-10-03 00:30:21 +0700

    Added animations

--- urllShortener2GIS/wwwroot/js/site.js
+++ urllShortener2GIS/wwwroot/js/site.js
@@ -2,10 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-const input = document.getElementById('input-url');
-const copyBtn = document.querySelector('.copy-btn');
-const form = document.getElementById('enter-form');
 
+const input = document.getElementById('input-url');
+const form = document.getElementById('enter-form');
+const copyBtn = document.querySelector('.copy-btn');
+const redirectBtn = document.querySelector('.redirect-link');
+
+
+redirectBtn.addEventListener('click', () => {
+    if (redirectBtn.href == '') {
+        const emptyLink = document.querySelector('.empty-link');
+        emptyLink.classList.add('empty-link-opened');
+    }
+})
+
+input.addEventListener('click', () => {
+    const emptyLink = document.querySelector('.empty-link');
+    emptyLink.classList.remove('empty-link-opened');
+})
 
 form.addEventListener('submit', (e) => {
     const warningEmpty = document.getElementById('empty');
@@ -25,14 +39,14 @@ form.addEventListener('submit', (e) => {
 
 });
 
-const matchURL = (url) => {
-    var expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
-    var regex = new RegExp(expression);
-    return url.match(regex) ? true : false;
-}
-
 copyBtn.addEventListener('click', () => {
     const copyText = document.querySelector('.input-url');
     copyText.select();
     document.execCommand('copy');
 })
+
+const matchURL = (url) => {
+    var expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+    var regex = new RegExp(expression);
+    return url.match(regex) ? true : false;
+}
